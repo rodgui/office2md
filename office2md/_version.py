f25edf109cbd928bb9c0ef9f7ffe10fd18eb8f95
Rodrigo <rod.gui@gmail.com>
2025-12-04 18:27:31 -0300

    feat: atualiza versão e commit_id para 0.2.0.post1

--- office2md/_version.py
+++ office2md/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.0.post6+g58c5c7a1d.d20251204'
-__version_tuple__ = version_tuple = (0, 0, 'post6', 'g58c5c7a1d.d20251204')
+__version__ = version = '0.2.0.post1+g531dab341.d20251204'
+__version_tuple__ = version_tuple = (0, 2, 0, 'post1', 'g531dab341.d20251204')
 
-__commit_id__ = commit_id = 'g58c5c7a1d'
+__commit_id__ = commit_id = 'g531dab341'
